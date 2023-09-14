f56e8416650a454f1047d8ab916bc46e4b693ccf
Vanessa Jesik <vajesik@gmail.com>
2023-09-14 16:18:19 -0600

    completed lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import ipdb
-
 class CashRegister:
     def __init__(self, discount=0):
         self.discount = discount
@@ -10,9 +8,8 @@ class CashRegister:
     def add_item(self, title, price, quantity = 1):
         self.total = self.total + (price * quantity)
         self.last_transaction = price * quantity
-        for quantity in range(quantity):
+        for q in range(quantity):
             self.items.append(title)
-#Why was it not functioning with self.last_transaction set below? but did work below when self.last_transaction = price
         
     def apply_discount(self):
         if self.discount:
@@ -24,6 +21,3 @@ class CashRegister:
     def void_last_transaction(self):
         self.total = self.total - self.last_transaction
         
-        
-# me = CashRegister()
-# me.add_item("bubblegum", 2, 3)
